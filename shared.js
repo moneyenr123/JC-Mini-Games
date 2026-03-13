2c49deef92afdb91c3982a48b482023c613401a7
JC Unblocked <158098394+moneyenr123@users.noreply.github.com>
2026-03-13 16:36:25 -0400

    Create shared.js

---
+++ shared.js
@@ -0,0 +1,23 @@
+// This script automatically adds the Favicon and Apple tags to any page it's on
+const head = document.head;
+
+const favicon = document.createElement('link');
+favicon.rel = 'icon';
+favicon.href = 'favicon.png';
+
+const appleIcon = document.createElement('link');
+appleIcon.rel = 'apple-touch-icon';
+appleIcon.href = 'favicon.png';
+
+const appleTitle = document.createElement('meta');
+appleTitle.name = 'apple-mobile-web-app-title';
+appleTitle.content = 'JC Mini Games';
+
+const appleCap = document.createElement('meta');
+appleCap.name = 'apple-mobile-web-app-capable';
+appleCap.content = 'yes';
+
+head.appendChild(favicon);
+head.appendChild(appleIcon);
+head.appendChild(appleTitle);
+head.appendChild(appleCap);
